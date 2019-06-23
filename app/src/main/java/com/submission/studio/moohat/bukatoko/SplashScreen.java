a5070c6fc65302a2542ee3e5c7ccf3d3f31a4392
moohat <refixzcorp@gmail.com>
2019-06-23 23:00:32 +0700

    Initial commit

---
+++ app/src/main/java/com/submission/studio/moohat/bukatoko/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.submission.studio.moohat.bukatoko;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int LENGTH = 1000; // 1000=1 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        },LENGTH);
+
+    }
+}
